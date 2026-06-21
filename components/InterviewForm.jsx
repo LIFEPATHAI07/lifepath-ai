5c5d34625c34d13c240cc10c5811d92f58d81639
LIFEPATHAI07 <sinanwealth@gmail.com>
2026-06-21 22:34:48 +0530

    Update InterviewForm.jsx

--- components/InterviewForm.jsx
+++ components/InterviewForm.jsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from "react";
-import { saveInterview } from "@/lib/feedback";
-import { getUserId } from "@/lib/session";
+import { saveInterview } from "../lib/feedback";
+import { getUserId } from "../lib/session";
 
 const QUESTIONS = [
   { key: "whyTried", label: "Why did you try LifePath AI today?" },
